1dbcaa54ace8186936d558fa312c6a213e3a0742
Jorge Cabrera <126417840+JorgeCabrera2003@users.noreply.github.com>
2026-02-03 21:38:27 -0400

    FEAT:  Agregar acción de visualización de PDF y vista detallada de personal

--- app/Filament/Resources/PersonalResource.php
+++ app/Filament/Resources/PersonalResource.php
@@ -116,6 +116,7 @@ class PersonalResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
